07b4e64dfb240959f9440be489bff1ba4267dfa9
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-16 15:50:18 -0700

    day 4

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from queue import Queue
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -84,60 +86,78 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        # if the current node is None
+        # we know we've reached the end of a recursion
+        # (base case) we want to return
+        if node is None:
+            return
+
+        # check if we can "move left"
+        if node.left is not None:
+            node.in_order_print(node.left)
+
+        # visit the node by printing its value
+        print(node.value)
+
+        # check if we can "move right"
+        if node.right is not None:
+            node.in_order_print(node.right)
+
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+
+    from collections import deque
+
     def bft_print(self, node):
-        pass
-        # go through node level by level
+        # You should import the queue class from earlier in the
+        # week and use that class to implement this method
+        # Use a queue to form a "line" 
+        # for the nodes to "get in"
+        # start by placing the root in the queue
+        queue = node.deque()
+        queue.append(node)
+        # need a while loop to iterate
+        # what are we checking in the while statement?
+        # while length of queue is greater than 0
+        while len(queue) > 0:
+            # dequeue item from front of queue
+            current = queue.popleft()
+            # print that item
+            print(current.value)
+            # place current item's left node in queue if not None
+            if current.left:
+                queue.append(current.left)
+            # place current item's right node in queue if not None
+            if current.right:
+                queue.append(current.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
+
     def dft_print(self, node):
         pass
-        # go far out to leaf from root of the tree
-#         current = node
+        # initialize an empty stack
+        stack = []
+        # push the root node onto the stack
+        stack.append(node)
 
-#         3
-#     /       \
-#   1          4
-# /            /    \
-# 0            4     8
+        # need a while loop to manager our iteration
+        # if stack is not empty enter the while loop
+        while len(stack) > 0:
+            # pop top item off the stack
+            current = stack.pop()
+            # print that item's value
+            print(current.value)
 
-
-
-#         1.
-#         print 3
-#         [] -> [1, 4]
-
-#         2.
-#         pop off 1
-#         print 1
-#         add 0
-#         [4] -> [0, 4]
-
-#         3.
-#         pop off 0
-#         print 0
-#         0 has no kids, add nothing
-
-        
-
-#         1.
-#         print 3
-#         [] -> [1, 4]
-
-#         2.
-#         pop off 1
-#         print 1
-#         add 0
-#         [4] -> [4, 0]
-
-#         3.
-#         pop off 4
-#         print 4
-#         add 4 and 8
-#         [0] -> [0, 4, 8]
+            # if there is a right subtree
+            if current.right:
+                # push right item onto the stack
+                stack.append(current.right)
+                
+            # if there is a left subtree
+            if current.left:
+                # push left item onto the stack
+                stack.append(current.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -9,7 +9,10 @@ class ListNode:
         self.next = next
 
     def delete(self):
-        pass
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
             
 """
 Our doubly-linked list class. It holds references to 
@@ -140,7 +143,7 @@ class DoublyLinkedList:
         self.length -= 1
 
         if self.head is None and self.tail is None:
-            return None
+            return 
 
         if self.head is self.tail:
             self.head = None
